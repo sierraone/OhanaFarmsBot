5c49e725547743e6a4ecf0d75c34c207760a1a08
sierraone <yinanwang@projectwhisper.com>
2019-04-28 22:45:54 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,29 +1,37 @@
-const Discord = require('discord.js');
+const eris = require('eris');
 
-const client = new Discord.Client();
+// Create a Client instance with our bot token.
+const bot = new eris.Client('my_token');
 
- 
-
-client.on('ready', () => {
-
-    console.log('I am ready!');
-
+// When the bot is connected and ready, log to console.
+bot.on('ready', () => {
+   console.log('Connected and ready.');
 });
 
- 
+// Every time a message is sent anywhere the bot is present,
+// this event will fire and we will check if the bot was mentioned.
+// If it was, the bot will attempt to respond with "Present".
+bot.on('messageCreate', async (msg) => {
+   const botWasMentioned = msg.mentions.find(
+       mentionedUser => mentionedUser.id === bot.user.id,
+   );
 
-client.on('message', message => {
-
-    if (message.content === 'ping') {
-
-       message.reply('pong');
-
+   if (botWasMentioned) {
+       try {
+           await msg.channel.createMessage('Present');
+       } catch (err) {
+           // There are various reasons why sending a message may fail.
+           // The API might time out or choke and return a 5xx status,
+           // or the bot may not have permission to send the
+           // message (403 status).
+           console.warn('Failed to respond to mention.');
+           console.warn(err);
        }
-
+   }
 });
 
- 
+bot.on('error', err => {
+   console.warn(err);
+});
 
-// THIS  MUST  BE  THIS  WAY
-
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+bot.connect();
